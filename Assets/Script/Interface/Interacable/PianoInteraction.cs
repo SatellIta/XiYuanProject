7c57b22f708622c204fd1c08554337ddfdcfece9
筱万 <16263879+xiao-wan12138@user.noreply.gitee.com>
2026-03-07 15:28:22 +0800

    添加了部分乐器以及电子琴的交互脚本

---
+++ Assets/Script/Interface/Interacable/PianoInteraction.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoInteraction : MonoBehaviour, IInteractable
+{
+
+    private bool beingPlayed = false;
+
+    // 对演奏界面UI的引用
+    [SerializeField] private GameObject pianoUI;
+    // 可选的玩家控制脚本，用于在演奏时禁用移动
+    [SerializeField] private MonoBehaviour playerController;
+    public string InteractionPrompt => beingPlayed ? "按E关闭" : "按E演奏";
+
+    public Transform Transform => transform;
+
+    public bool CanInteract()
+    {
+        return true;
+    }
+
+    public void Interact()
+    {
+        if (!beingPlayed)
+        {
+            EnterPianoMode();
+        }
+        else
+        {
+            ExitPianoMode();
+        }
+        beingPlayed = !beingPlayed;
+    }
+
+    private void EnterPianoMode()
+    {
+        if (pianoUI != null)
+        {
+            pianoUI.SetActive(true);
+        }
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (playerController != null)
+        {
+            playerController.enabled = false;
+        }
+    }
+
+    private void ExitPianoMode()
+    {
+        if (pianoUI != null)
+        {
+            pianoUI.SetActive(false);
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        if (playerController != null)
+        {
+            playerController.enabled = true;
+        }
+    }
+}
